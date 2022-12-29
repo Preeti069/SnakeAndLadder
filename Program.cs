5e15ee326d4637b764cb5ee1754f763ec5c126db
preeti <pritich0896@gmail.com>
2022-12-29 14:27:04 +0530

    WelcomeMessage

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadderProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladder Program");
+            //SnakeLadder.SinglePlayer();
+
+            Console.WriteLine();
+        }
+    }
+}
