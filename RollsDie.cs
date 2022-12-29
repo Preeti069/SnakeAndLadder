5af79507ea048e3cca32dfebb85b4e95a5e01b23
preeti <pritich0896@gmail.com>
2022-12-29 19:48:51 +0530

    Player Rolls The Die To get number between 1to6

---
+++ RollsDie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadderProblem
+{
+    public class RollsDie
+    {
+        public static void Rolls()
+        {
+            int position = 0;
+            Console.WriteLine("Start Positon is : " + position);
+            Random random = new Random();
+            int dicethrown = random.Next(1, 7);
+            Console.WriteLine("\n Dies Output is : " + dicethrown);
+        }
+    }
+}
