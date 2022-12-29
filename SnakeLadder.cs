2bc9902bb610f5ae4a954be2453ccdc3ad3d47f4
preeti <pritich0896@gmail.com>
2022-12-29 14:31:51 +0530

    UC1_added

---
+++ SnakeLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadderProblem
+{
+    public class SnakeLadder
+    {
+        public static void SinglePlayer()
+        {
+                int position = 0;
+                Console.WriteLine("Start Positon is : " + position);
+            
+        }
+    }
+}
