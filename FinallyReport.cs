98ad57be7cfa8f26181c87fb87e99e5339cc5d48
preeti <pritich0896@gmail.com>
2022-12-30 11:02:07 +0530

    Adding Player 2

---
+++ FinallyReport.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadderProblem
+{
+    public class FinallyReport
+    {
+        public static int mPlayerPosition(int PlayerPosition)
+        {
+            Random random = new Random();
+            int Roll_die = random.Next(1, 7);
+            int Option = random.Next(3);
+
+            PlayerPosition += Roll_die;
+
+            if (Option == 0)
+            {
+                PlayerPosition = PlayerPosition - Roll_die;
+                Console.WriteLine("snake " + PlayerPosition);
+            }
+            else if (Option == 1)
+            {
+                PlayerPosition = PlayerPosition + Roll_die;
+                Console.WriteLine("ladder " + PlayerPosition);
+            }
+            else
+            {
+                Console.WriteLine("no play " + PlayerPosition);
+            }
+            if (PlayerPosition > 100) PlayerPosition -= Roll_die;
+            if (PlayerPosition < 0) PlayerPosition = 0;
+
+            return PlayerPosition;
+
+        }
+
+    }
+}
