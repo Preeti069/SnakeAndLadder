a45db68927f04559571ec651d36a9cd5f391da37
preeti <pritich0896@gmail.com>
2022-12-30 11:06:28 +0530

    Adding player 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace SnakeandLadderProblem
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             //Console.WriteLine("Welcome To Snake And Ladder Program");
             //SnakeLadder.SinglePlayer();
@@ -16,9 +17,35 @@ namespace SnakeandLadderProblem
             //ChecksForOption.SnakeLadder();
             //TillWinning.TillPosition100();
             //ExactWinning.Winning();
-              Position.DieCountTillWin();
+            //Position.DieCountTillWin();
 
-            Console.WriteLine();
+
+            Console.WriteLine("Snake & Ladder Simulator");
+
+            int PlayerPosition_1 = 0, PlayerPosition_2 = 0;
+
+
+            Console.WriteLine("The Player has started from 0 possition");
+
+            while (true)
+            {
+                PlayerPosition_1 = FinallyReport.mPlayerPosition(PlayerPosition_1);
+                Console.WriteLine("player 1 position " + PlayerPosition_1);
+
+                PlayerPosition_2 = FinallyReport.mPlayerPosition(PlayerPosition_2);
+                Console.WriteLine("player 2 position " + PlayerPosition_2);
+                if (PlayerPosition_1 == 100)
+                {
+                    Console.WriteLine("player 1 has won ");
+                    break;
+                }
+                else if (PlayerPosition_2 == 100)
+                {
+                    Console.WriteLine("player 2 has won ");
+                    break;
+                }
+            }
+
         }
     }
 }
